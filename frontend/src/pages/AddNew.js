4d2e492083a7655b9dfecc967a3e1f7c642a8b1f
Daniel Lee <rjalths0303@gmail.com>
2022-10-16 03:34:26 -0400

    least info post request made

--- frontend/src/pages/AddNew.js
+++ frontend/src/pages/AddNew.js
@@ -1,16 +1,62 @@
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Col, Container } from "react-bootstrap";
+import Arrow from "react-arrow";
+import axios from "axios";
 
 function AddNew() {
+  const [state, setState] = useState({
+    user: 0,
+    apartment: 0,
+    flat_type: 0,
+    contract_date: "2022-10-10",
+    price: 0,
+    lease_type: "",
+    lease_term: "",
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.controlId]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    const leaseData = {
+      user_id: 9,
+      apartment_id: 1,
+      flat_type: 0,
+      contract_date: "2022-10-10",
+      price: 10000,
+      lease_type: "One Year Lease",
+      lease_term: "2023-2024",
+    };
+
+    console.log(leaseData);
+    axios
+      .post("http://127.0.0.1:8000/apartments/create_lease_data/", leaseData, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((response) => {
+        console.log(response.status);
+        console.log(response.data);
+      });
+  };
+
   return (
     <Container>
       <Col>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="apartment">
             <Form.Label>Your Apartment</Form.Label>
             <br></br>
-            <Form.Select>
+            <Form.Select onChange={handleChange}>
               <option value="center">Collegetown Center</option>
               <option value="plaza">Collegetown Plaza</option>
               <option value="terrace">Collegetown Terrace</option>
@@ -19,10 +65,10 @@ function AddNew() {
               <option value="na">Doesn't see your apartment?</option>
             </Form.Select>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="bedrooms">
+          <Form.Group className="mb-3" controlId="flat_type">
             <Form.Label>Number of Bedrooms</Form.Label>
             <br></br>
-            <Form.Select>
+            <Form.Select onChange={handleChange}>
               <option value="studio">Studio</option>
               <option value="one">1 Bedroom</option>
               <option value="two">2 Bedrooms</option>
@@ -31,27 +77,26 @@ function AddNew() {
               <option value="five">Input manually</option>
             </Form.Select>
           </Form.Group>
-
-          <Form.Group className="mb-3" controlId="contract date">
+          <Form.Group className="mb-3" controlId="contract_date">
             <Form.Label>Contract Date</Form.Label>
-            <Form.Control type="date" />
+            <Form.Control type="date" onChange={handleChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="price">
             <Form.Label>Price</Form.Label>
             <Form.Control type="number" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="lease type">
+          <Form.Group className="mb-3" controlId="lease_type">
             <Form.Label>Lease Type</Form.Label>
             <br></br>
-            <Form.Select>
+            <Form.Select onChange={handleChange}>
               <option value="one-year">One Year Lease</option>
               <option value="sublet">Sublet</option>
             </Form.Select>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="lease term">
+          <Form.Group className="mb-3" controlId="lease_term">
             <Form.Label>Lease Term</Form.Label>
             <br></br>
-            <Form.Select>
+            <Form.Select onChange={handleChange}>
               <option value="2023-24">2023-2024</option>
               <option value="2022-23">2022-2023</option>
             </Form.Select>
