ef42ba0dde9d164d494fba890442057ad797408b
choikh0423 <kyuhwanchoi0423@gmail.com>
2022-10-15 20:44:25 -0400

    backend implementation bug fix

--- backend/bigredapp/main/serializers.py
+++ backend/bigredapp/main/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from .models import Apartment, Statistics
+from .models import Apartment, LeaseData, Statistics
 from rest_framework import serializers
 from datetime import datetime
 
@@ -17,17 +17,14 @@ class CreateUserSerializer(serializers.HyperlinkedModelSerializer):
         # Tuple of serialized model fields (see link [2])
         fields = [ "email", "password", ]
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
 class ApartmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Apartment
         fields = ['name', 'address', 'rating', 'number_of_reviews', 'gym', 'laundry', 'free_wifi_router', 'free_electricity' ]
 
-class StatisticsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Statistics
-        field = ["one_year_data", "two_year_data", "three_year_data"]
+class LeaseDataSerializer(serializers.Serializer):
+    # initialize fields
+    contract_date = serializers.DateField()
+    lease_term = serializers.CharField()
+    lease_type = serializers.CharField()
+    price = serializers.IntegerField()
